ae3c2a31229d01dd6244031ac00f519c9c14e480
Garv Gaba <121475626+GarvGaba@users.noreply.github.com>
2023-10-08 21:38:01 +0530

    Add files via upload

---
+++ Lecture3 II.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x;
+    int y;
+
+    x=89;
+    y=234;
+
+    int c = x+y;
+
+    cout<<x<<endl<<y<<endl<<c;
+
+
+     cout<<"This is usage of endl"<<endl<<"this line will be printed in next line because we used endl here";
+
+    return 0;
+}
