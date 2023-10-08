732ff4ec4b886aba562d0525bd13264b97f17c89
Garv Gaba <121475626+GarvGaba@users.noreply.github.com>
2023-10-08 21:05:45 +0530

    Add files via upload

---
+++ Lecture_1.cpp
@@ -0,0 +1,4 @@
+In this lecture the mentor have given the brief description of C++.
+Some basic things like, What are advantages of C++ over other programming languages,
+Importance of STL in C++, and
+How the programming part of this course should be practiced.
