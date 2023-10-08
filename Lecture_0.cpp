01f5bc4e76e10d0350d43b2d7a84e02ce6bc9c58
Garv Gaba <121475626+GarvGaba@users.noreply.github.com>
2023-10-08 21:05:05 +0530

    Add files via upload

---
+++ Lecture_0.cpp
@@ -0,0 +1,14 @@
+//In Zero lecture the mentor just have given the overview of the cousre and made us download ths VS Code ide .
+ 
+
+
+//This is the first code  in C++ .
+
+
+
+#include<iostream>
+using namespace std;
+int main(){
+  cout<<"This is the starting of my journey of Cipher Schools C++ and DSA course";
+  return 0;
+}
